1d9e18249c6b8f36758f7f38312dba1ea6769973
Badri Narayanan <rbadri7501@gmail.com>
2022-05-06 13:01:50 +0530

    JSX

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
 
+var stud = [{name: "Ajay", roll: 1, mark1: 90, mark2: 59, mark3: 82, total:0,},
+              {name: "Balu", roll: 2, mark1: 84, mark2: 93, mark3: 63, total:0},];
+
+const Test = () =>{
+  return(
+    stud.map(item=>(
+      <p>
+        <span>Name: {item.name} </span>
+        <span>Roll: {item.roll} </span>
+        <span>Total: {item.total= item.mark1 + item.mark2 + item.mark3} </span>
+      </p>
+    ))
+  )
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <h1>Hello World</h1>
+      <Test />
+    </div>
+  );
+}
+
+export default App;
+
+{/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,9 +40,4 @@ function App() {
         >
           Github Profile
         </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+      </header> */}
